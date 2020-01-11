98d6893cede7bb1e07c124fb173adf86d15fe577
rpharr <rpharr@richmond.edu>
2020-01-11 01:09:51 -0500

    styled large sceens and ajusted small

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,28 @@
-autoplay();
-
-function autoplay() {
-    $('.carousel').carousel('next');
-    setTimeout(autoplay, 10000);
-}
-
 $(document).ready(function() {
     $('.sidenav').sidenav();
+
+    var interval;
+
+    function initCarousel() {
+        let h = $(".carocontainer")[0].offsetHeight;
+
+        $('.carousel.carousel-slider').carousel({
+            onCycleTo: function(ele) {
+                clearInterval(interval);
+                interval = setInterval(function() {
+                    $('.carousel.carousel-slider').carousel('next');
+                }, 10000);
+            },
+            fullWidth: true,
+            indicators: true
+        }).height(`${h}px`);
+    }
+
+    $(window).resize(function() {
+        initCarousel();
+
+    });
+
+    initCarousel();
+
 });
